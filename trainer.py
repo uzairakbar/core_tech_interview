651a66937e99a0835f596de9f5554dbca17cb2b2
Uzair Akbar <uzairakbar025@gmail.com>
2023-05-26 11:01:05 +0200

    add some plots, clean some log prints

--- trainer.py
+++ trainer.py
@@ -8,7 +8,7 @@ from utils import class_weights
 def trainer(
     G, train_idx, val_idx, test_idx, labels,
     hidden_size, num_hidden, lr, weight_decay, epochs, early_stopping=True,
-    verbose=True, plot_loss=False):
+    verbose=True, plot_loss=False, plot_norm=False):
     model = HeteroRGCN(
         G, 
         in_size=10, hidden_size=hidden_size, out_size=3, 
@@ -49,6 +49,8 @@ def trainer(
                 norm += p.norm().item()
 
         if best_val_loss > val_loss and early_stopping:
+            # early-stopping: pick best model based on val_loss
+            best_train_loss = loss.item()
             best_val_acc = val_acc
             best_test_acc = test_acc
             best_val_loss = val_loss
@@ -57,6 +59,7 @@ def trainer(
             best_norm = norm
             best_epoch = epoch
         elif not early_stopping:
+            best_train_loss = loss.item()
             best_val_acc = val_acc
             best_test_acc = test_acc
             best_val_loss = val_loss
@@ -65,25 +68,19 @@ def trainer(
             best_norm = norm
             best_epoch = epoch
 
-
         opt.zero_grad()
         loss.backward()
         opt.step()
 
         if epoch % (epochs//10) == 0 and verbose:
-            print('Epoch %d, Train Loss %.4f, Train Acc %.4f, Val Loss %.4f (Best %.4f), Val Acc %.4f (Best %.4f), Test Acc %.4f (Best %.4f), Norm %.4f (Best %.4f)' % (
-                epoch,
-                loss.item(),
-                train_acc.item(),
-                val_loss,
-                best_val_loss,
-                val_acc.item(),
-                best_val_acc.item(),
-                test_acc.item(),
-                best_test_acc.item(),
-                norm,
-                best_norm
-            ))
+            print(
+                f"Epoch {epoch}, "
+                f"Train Loss {loss.item():.4f} (Best {best_train_loss:.4f}), "
+                f"Val Loss {val_loss:.4f} (Best {best_val_loss:.4f}), "
+                f"Val Acc {val_acc:.4f} (Best {best_val_acc:.4f}), "
+                f"Test Acc {test_acc:.4f} (Best {best_test_acc:.4f}), "
+                f"Norm {norm:.4f} (Best {best_norm:.4f}), "
+            )
         
         log_norm.append(norm)
         log_train_loss.append(loss.item())
@@ -95,9 +92,33 @@ def trainer(
         plt.axvline(best_epoch, color='r')
         plt.xlabel("epoch")
         plt.ylabel("loss")
-        plt.legend(["train", "val", "best"])
-        plt.title(f"Training Config: #h-layers: {num_hidden}, #h-features: {hidden_size}, lr: {lr:.4f}, weight_decay: {weight_decay:.4f}")
+        plt.legend(["train", "val", "selected"])
+        plt.title(
+            f"Training Config: #h-layers: {num_hidden}, "
+            f"#h-features: {hidden_size}, "
+            f"lr: {lr:.4f}, "
+            f"weight_decay: {weight_decay:.4f}"
+        )
         plt.grid()
 
-    print(f"""TRAINING RESULT -- Train Acc: {best_train_acc:.4f}, Val Acc: {best_val_acc:.4f}, Test Acc: {best_test_acc:.4f}.\n""")
+    if plot_norm:
+        plt.plot(log_norm)
+        plt.axvline(best_epoch, color='r')
+        plt.xlabel("epoch")
+        plt.ylabel("L2 Norm of Noise Perturbation Edge Weights")
+        plt.legend(["norm", "selected"])
+        plt.title(
+            f"Training Config: #h-layers: {num_hidden}, "
+            f"#h-features: {hidden_size}, "
+            f"lr: {lr:.4f}, "
+            f"weight_decay: {weight_decay:.4f}"
+        )
+        plt.grid()
+
+    print(
+        f"TRAINING RESULT -- "
+        f"Train Acc: {best_train_acc:.4f}, "
+        f"Val Acc: {best_val_acc:.4f}, "
+        f"Test Acc: {best_test_acc:.4f}.\n"
+    )
     return best_model.eval(), best_train_acc, best_val_acc, best_test_acc, best_val_loss
